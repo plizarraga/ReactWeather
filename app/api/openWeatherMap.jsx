6cd23df5c94ecc762903a88ae34e3d507f13d2b8
Pedro Lizarraga <plizarraga@fender.com>
2017-01-22 00:09:15 -0800

    Initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,20 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_UR = 'http://api.openweathermap.org/data/2.5/weather?appid=07c4f0968db0815305dfa97002d3b29a&units=imperial';
+
+module.exports = {
+  getTemp: function(location){
+    var encodedLocation = encodeURIComponent(location);
+    var requerstURL = `${OPEN_WEATHER_MAP_UR}&q=${encodedLocation}`;
+
+    return axios.get(requerstURL).then(function(res){
+      if(res.data.cod && res.data.message){
+        throw new Error(res.data.message);
+      }else{
+        return res.data.main.temp;
+      }
+    }).catch(function(res){
+      throw new Error(res.response.data.message);
+    })
+  }
+}
---
+++ app/components/Examples.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Examples = () => {
+    return (
+        <h2>Examples Component</h2>
+    )
+  }
+
+module.exports = Examples;
