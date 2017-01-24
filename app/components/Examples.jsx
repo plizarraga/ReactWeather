05f8f8cee8b4b0bdcc9dc44b658a7f9d9ceabcf0
Pedro Lizarraga <plizarraga@fender.com>
2017-01-23 22:27:42 -0800

    Style to about, examples pages

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,20 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = () => {
     return (
-        <h2>Examples Component</h2>
+      <div>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are a few example locations to try out:</p>
+        <ol>
+          <li>
+            <Link to="/?location=LA">Los Angeles, CA</Link>
+          </li>
+          <li>
+            <Link to="/?location=Rio">Rio, Brazil</Link>
+          </li>
+        </ol>
+      </div>
     )
   }
 
